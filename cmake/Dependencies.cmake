004dcc7c52e48ecc8ebadf464928377230f8939f
Timur Dzhesur <61432929+Land-Maze@users.noreply.github.com>
2025-11-06 22:22:10 +0100

    fix(cmake): fix the imgui backend fetching

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -28,7 +28,7 @@ add_library(imgui_lib
 		${imgui_SOURCE_DIR}/imgui_tables.cpp
 		${imgui_SOURCE_DIR}/imgui_widgets.cpp
 		${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.cpp
-		${imgui_SOURCE_DIR}/backends/imgui_impl_opengl3.cpp
+		${imgui_SOURCE_DIR}/backends/imgui_impl_wgpu.cpp
 )
 target_include_directories(imgui_lib PUBLIC
 		${imgui_SOURCE_DIR}
